c3a886805b035b408cd43a3812d49205664d92b0
elihai14 <elihaiafuta@gmail.com>
2025-06-19 19:03:05 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+const members = [
+    {
+        name: "Ori Abutbul",
+        age: 22,
+        address: "Nahariya",
+        number: "0523336625",
+        imgSrc: "./images/person1.jpg"
+    },
+    {
+        name: "Ram Turjman",
+        age: 36,
+        address: "Nahariya",
+        number: "0552263345",
+        imgSrc: "./images/person1.jpg"
+    },
+    {
+        name: "Rafi Refael",
+        age: 25,
+        address: "Nahariya",
+        number: "0502525886",
+        imgSrc: "./images/person1.jpg"
+    }
+];
+
+const memberList = document.getElementById('members');
+function createMemberElement(item,index){
+ const li = document.createElement('li');
+    li.innerHTML =  `<img src="${item.imgSrc}" alt="" width="80" height="80">${item.name} <button class="details"> <img src="./images/details_icon.png" alt="" width="20" height="20"></button>
+                <button class="edit"> <img src="./images/edit_icon.png" alt="" width="20" height="20"></button> <button class="delete"><img src="./images/deletIcon.png" alt="" width="20" height="20"></button>`
+    li.setAttribute('data-id', index)
+                return li;
+}
+members.forEach((item , index) => {
+    const memberElement = createMemberElement(item, index)
+    memberList.appendChild(memberElement)
+})
